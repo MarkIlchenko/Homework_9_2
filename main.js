22850983e4193535fa68b625926d0f8fdf01affd
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-01-23 19:23:08 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// Homework 2
+let arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4];
+let index = arr.reduce((a, b, i) => b < arr[a] ? i : a, 0);
+document.write(`Min: ${arr[index]} Position: ${index}`);
